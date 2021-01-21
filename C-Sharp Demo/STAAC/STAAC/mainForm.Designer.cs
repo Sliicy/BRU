ad04953a537acd638878ee7556af9f04203d1be8
Sliicy <Sliicy@users.noreply.github.com>
2021-01-21 17:43:51 -0500

    Add Emphasis

--- C-Sharp Demo/STAAC/STAAC/mainForm.Designer.cs
+++ C-Sharp Demo/STAAC/STAAC/mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace STAAC {
-    partial class mainForm {
+    partial class MainForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -24,21 +24,61 @@ namespace STAAC {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.coolDown = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // mainForm
+            // btnHelp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnHelp.Location = new System.Drawing.Point(15, 15);
+            this.btnHelp.Margin = new System.Windows.Forms.Padding(6);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(150, 44);
+            this.btnHelp.TabIndex = 0;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.BtnHelp_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Come here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnHelp_Click);
+            // 
+            // coolDown
+            // 
+            this.coolDown.Interval = 5000;
+            this.coolDown.Tick += new System.EventHandler(this.CoolDown_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "mainForm";
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHelp);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "STAAC";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer coolDown;
     }
 }
 
--- C-Sharp Demo/STAAC/STAAC/mainForm.cs
+++ C-Sharp Demo/STAAC/STAAC/mainForm.cs
@@ -7,11 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
 
 namespace STAAC {
-    public partial class mainForm : Form {
-        public mainForm() {
+    public partial class MainForm : Form {
+
+        // List of words that the user is angry about (deliberately repeatedly pressing):
+        List<string> emphasizable = new List<string>();
+
+        public MainForm() {
             InitializeComponent();
+        }
+
+        SpeechSynthesizer narrator;
+
+        private void MainForm_Load(object sender, EventArgs e) {
+
+        }
+        
+        private void BtnHelp_Click(object sender, EventArgs e) {
+            narrator = new SpeechSynthesizer();
+            Button buttonPressed = (Button)sender;
+            if (emphasizable.Contains(buttonPressed.Text)) {
+                narrator.SpeakSsmlAsync("<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\"><emphasis>" + buttonPressed.Text + "</emphasis></speak>");
+            } else {
+                narrator.SpeakAsync(buttonPressed.Text);
+                emphasizable.Add(buttonPressed.Text);
+                coolDown.Start();
+            }
+            
+            
+        }
+
+        private void CoolDown_Tick(object sender, EventArgs e) {
+            // Pop first word from emphasizable list:
+            if (emphasizable.Count > 0) emphasizable.RemoveAt(0);
         }
     }
 }
