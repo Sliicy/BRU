a23dff569a4581e613384ae1f1cc2ca0ab2fdeda
Sliicy <Sliicy@users.noreply.github.com>
2021-04-12 19:27:43 -0400

    Add Models Comments

--- C-Sharp Demo/STAAC/STAAC/Models/FolderDates.cs
+++ C-Sharp Demo/STAAC/STAAC/Models/FolderDates.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 
+/**
+ * Model used to store information about folder dates.
+ * This is used when searching the Template Folder for sorting by the most recently modified date:
+ */
 namespace STAAC.Models {
     class FolderDates {
+
+        // Each folder has a name:
         public string Name { get; set; }
+        
+        // Each folder has the last date it was modified:
         public DateTime Modified { get; set; }
     }
 }
--- C-Sharp Demo/STAAC/STAAC/Models/TemplateModel.cs
+++ C-Sharp Demo/STAAC/STAAC/Models/TemplateModel.cs
@@ -5,29 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Model used to store information about a Template.
+ * Each Template has a name, author, category, color scheme, matrix data, & dimensions for building that matrix.
+ */
 namespace STAAC.Models {
     public class TemplateModel {
 
+        // Holds the name of the Template:
         private string name;
+
+        // Holds the author's name:
         private string author;
+        
+        // Holds the category name:
         private string category;
+        
+        // Holds the color scheme to use for the template:
         private string colorScheme;
 
-        // String representation of matrix:
+        // Holds a string containing the layout of the buttons, separated by commas, and prepended with "Matrix Data=":
         private string matrixData;
 
-        // Contains dimensions of matrix:
+        // Holds the dimensions used to build the matrix:
         private int matrixWidth = 1;
         private int matrixHeight = 1;
 
+        // Constructor with only a name required:
         public TemplateModel(string name) {
             this.name = name;
             colorScheme = "None";
         }
 
+        // Constructor with name, and dimensions to use for the Template:
         public TemplateModel(string name, int matrixWidth, int matrixHeight) {
             this.name = name;
+
+            // Default color scheme is none:
             colorScheme = "None";
+
             if (matrixWidth > 0) {
                 this.matrixWidth = matrixWidth;
             } else {
@@ -45,10 +61,9 @@ namespace STAAC.Models {
         }
 
         public void SetName(string newName) {
-            if (newName != null) {
+            if (newName != null)
                 if (newName.Length > 0)
                     name = newName;
-            }
         }
 
         public string GetAuthor() {
