4b4cecb1ee6c20211d36ad0433fe5f9045fb7bb7
Sliicy <Sliicy@users.noreply.github.com>
2021-04-12 20:23:17 -0400

    Add Comments to PhraseBuildingForm

--- C-Sharp Demo/STAAC/STAAC/templateForm.cs
+++ C-Sharp Demo/STAAC/STAAC/templateForm.cs
@@ -13,30 +13,35 @@ using System.IO;
 using System.Diagnostics;
 using STAAC.Models;
 
+/**
+ * Class responsible for displaying buttons to use from the template, as well as editing those buttons, generating audio,
+ * and everything else STAAC promises.
+ */
 namespace STAAC {
     public partial class TemplateForm : Form {
 
-        // List of words that the user is angry about (deliberately repeatedly pressing):
+        // List of words that the user is deliberate about (deliberately repeatedly pressing any specific button):
         readonly List<string> emphasizable = new List<string>();
 
-        // Controls whether the template is in edit mode:
+        // Controls whether the template buttons can be edited or not:
         bool editMode = false;
 
-        // Controls whether mouse is dragging a button to re-arrange it:
+        // Controls whether the mouse is dragging a button to re-arrange it:
         bool mouseDragging = false;
         Point dragLocation = new Point();
         Point oldButtonLocation = new Point();
 
-        // Contains model of each template:
+        // Contains a model of each template:
         readonly TemplateModel bm = new TemplateModel(MenuForm.selectedTemplate);
 
+        // Holds the dimensions used to calculate the button's size for height and width:
         int buttonWidth = 0;
         int buttonHeight = 0;
 
         // Speech object used to speak:
         SpeechSynthesizer narrator;
 
-        // Remember the last phrase just spoken:
+        // String to remember the last phrase just spoken:
         string lastPhraseSpoken = "";
 
         // Controls whether to clear the speech box:
@@ -47,7 +52,7 @@ namespace STAAC {
         }
 
         /*
-         * Load a bitmap without locking it:
+         * Load a bitmap without the file becoming locked:
          * Source: http://csharphelper.com/blog/2014/07/load-images-without-locking-their-files-in-c/
         */
         public static Bitmap LoadBitmapUnlocked(string fileName) {
@@ -62,16 +67,23 @@ namespace STAAC {
             buttonWidth = pnlButtons.Width / bm.GetMatrixWidth();
             buttonHeight = pnlButtons.Height / bm.GetMatrixHeight();
 
+            // These values increment as buttons are added. They are used to properly space everything:
             int countX = 0;
             int countY = 0;
+
+            // For each word found in the settings file, delimited by commas (coming from the "Matrix Data=" entrypoint):
             foreach (string segment in line.Split('=')[1].Split(',')) {
 
+                // Create a new button to project onto the form:
                 Button newButton = new Button {
+
+                    // Assign the text and sizes obtained from the settings file:
                     Text = segment,
                     Size = new Size(buttonWidth, buttonHeight),
                     Location = new Point(buttonWidth * countX, buttonHeight * countY)
                 };
 
+                // Check for the appropriate colors for all the buttons to follow:
                 if (bm.GetColorScheme().Length > 0) {
                     switch (bm.GetColorScheme().ToLower()) {
                         case "none":
@@ -82,18 +94,19 @@ namespace STAAC {
                     }
                 }
 
-                // Assign all buttons' click events, and drag events:
+                // Assign all buttons' click events, and drag events (so they can all dynamically be edited):
                 newButton.Click += new EventHandler(ButtonClick);
                 newButton.MouseDown += new MouseEventHandler(ButtonMouseDown);
                 newButton.MouseMove += new MouseEventHandler(ButtonMouseMove);
                 newButton.MouseUp += new MouseEventHandler(ButtonMouseUp);
 
-                // Add .PNG image if exists:
+                // Add a .PNG image if one exists for that particular button:
                 AddPicture(segment, newButton);
 
-                // Add button to the template:
+                // Finally, add button to the template:
                 pnlButtons.Controls.Add(newButton);
 
+                // Keep counting horizontally, and then vertically:
                 countX++;
                 if (countX > bm.GetMatrixWidth() - 1) {
                     countX = 0;
@@ -103,13 +116,15 @@ namespace STAAC {
         }
 
         private static void AddPicture(string filename, Button button) {
+
+            // Load the appropriate image from the Template folder given the name of the button, if it exists:
             if (File.Exists(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate, filename + ".png"))) {
                 button.BackgroundImage = LoadBitmapUnlocked(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate, filename + ".png"));
                 button.BackgroundImageLayout = ImageLayout.Zoom;
             }
         }
 
-        // Function to remember changes made to buttons:
+        // Function used to redraw the matrix of buttons:
         void RecalculateMatrix() {
             bm.SetMatrixData("Matrix Data=");
 
@@ -127,7 +142,7 @@ namespace STAAC {
             bm.SetMatrixData(bm.GetMatrixData().Remove(bm.GetMatrixData().Length - 1, 1));
         }
 
-        // This function saves all changes to file:
+        // Method to save all changes made to the settings file:
         void SaveSettings() {
             if (!Directory.Exists(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate))) {
                 Directory.CreateDirectory(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate));
@@ -142,6 +157,7 @@ namespace STAAC {
         }
 
         private void MainForm_Load(object sender, EventArgs e) {
+
             // Insert template name into window title:
             Text = "STAAC - " + MenuForm.selectedTemplate;
 
@@ -150,31 +166,33 @@ namespace STAAC {
                 Directory.CreateDirectory(Path.Combine(Application.StartupPath, MenuForm.WORDLISTS_FOLDER_NAME));
             }
 
-
             // Try to load appropriate template:
             if (Directory.Exists(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate))) {
                 if (File.Exists(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate, MenuForm.SETTINGS_FILE_NAME))) {
 
+                    // Load all settings found based on predefined entrypoints from settings file (case-insensitive):
                     foreach (string line in File.ReadAllLines(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate, MenuForm.SETTINGS_FILE_NAME))) {
                         if (line.ToLower().Contains("author")) {
                             bm.SetAuthor(line.Split('=')[1]);
                         } else if (line.ToLower().Contains("category")) {
                             bm.SetCategory(line.Split('=')[1]);
                         } else if (line.ToLower().Contains("matrix size")) {
+
                             // Read Matrix size:
                             bm.SetMatrixWidth(int.Parse(line.Split('=')[1].Split('x')[0]));
                             bm.SetMatrixHeight(int.Parse(line.Split('=')[1].Split('x')[1]));
 
                         } else if (line.ToLower().Contains("color scheme")) {
-
                             bm.SetColorScheme(line.Split('=')[1]);
                         } else if (line.ToLower().Contains("matrix data")) {
+
                             // Read all the button names and create buttons for each one:
                             bm.SetMatrixData(line);
                             ReloadButtons(bm.GetMatrixData());
                         }
                     }
                 } else {
+
                     // Settings file wasn't found. Abort:
                     MessageBox.Show("Couldn't find " + MenuForm.SETTINGS_FILE_NAME + " inside of " + Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate) + "!",
                         "Error", MessageBoxButtons.OK,
@@ -185,12 +203,16 @@ namespace STAAC {
         }
         
         private void BtnHelp_Click(object sender, EventArgs e) {
+
+            // Create a Speech object to pronounce words based on their text contents:
             narrator = new SpeechSynthesizer();
             Button buttonPressed = (Button)sender;
             if (emphasizable.Contains(buttonPressed.Text.Replace("&", ""))) {
                 narrator.SpeakSsmlAsync("<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\"><emphasis>" + buttonPressed.Text.Replace("&", "") + "</emphasis></speak>");
             } else {
                 narrator.SpeakAsync(buttonPressed.Text.Replace("&", ""));
+
+                // Add the word to emphasizable list, which is also removed from said list after a few seconds:
                 emphasizable.Add(buttonPressed.Text.Replace("&", ""));
                 coolDown.Start();
             }
@@ -198,14 +220,17 @@ namespace STAAC {
 
         void ButtonClick(object sender, EventArgs e) {
 
-            // Erase old words if new phrase:
+            // Erase old words if "Clear" is pressed:
             if (clearTextOnNextPress) {
                 txtSpeechBar.Clear();
             }
             clearTextOnNextPress = false;
             
+            // Check if edit mode is on or off:
             Button buttonPressed = (Button)sender;
             if (editMode) {
+                
+                // Allow the button to be edited:
                 if (oldButtonLocation == buttonPressed.Location) {
                     EditButtonForm n = new EditButtonForm(buttonPressed.Text);
                     n.ShowDialog();
@@ -215,6 +240,7 @@ namespace STAAC {
                     n.Dispose();
                 }
             } else {
+
                 // Process any special words that have dictionaries associated with them:
                 if (File.Exists(Path.Combine(Application.StartupPath, MenuForm.WORDLISTS_FOLDER_NAME, buttonPressed.Text + ".txt"))) {
                     var wordForm = new PhraseBuildingForm(buttonPressed.Text);
@@ -229,6 +255,7 @@ namespace STAAC {
         }
 
         void ButtonMouseDown(object sender, MouseEventArgs e) {
+
             // Responsible for initiating a button drag:
             if (editMode) {
                 mouseDragging = true;
@@ -243,6 +270,7 @@ namespace STAAC {
         }
 
         void ButtonMouseMove(object sender, MouseEventArgs e) {
+
             // Responsible for processing the button drag:
             if (mouseDragging) {
                 Button buttonDragged = (Button)sender;
@@ -252,6 +280,7 @@ namespace STAAC {
         }
 
         void ButtonMouseUp(object sender, MouseEventArgs e) {
+
             // Stops the button drag:
             mouseDragging = false;
             Button buttonDragged = (Button)sender;
@@ -260,7 +289,7 @@ namespace STAAC {
             buttonDragged.Left = (int)(Math.Round(buttonDragged.Left / (double)buttonDragged.Width, 0) * buttonDragged.Width);
             buttonDragged.Top = (int)(Math.Round(buttonDragged.Top / (double)buttonDragged.Height, 0) * buttonDragged.Height);
             
-            // Check if any button beneath:
+            // Check if any button beneath the button that was dragged:
             foreach (Control c in pnlButtons.Controls) {
                 if (c.Location == buttonDragged.Location && buttonDragged != c) {
 
@@ -271,11 +300,13 @@ namespace STAAC {
         }
 
         private void CoolDown_Tick(object sender, EventArgs e) {
+
             // Pop first word from emphasizable list:
             if (emphasizable.Count > 0) emphasizable.RemoveAt(0);
         }
 
         private void BtnOnScreenKeyboard_Click(object sender, EventArgs e) {
+
             // Kills any on-screen keyboards, and then launches a new one:
             var tabProcesses = Process.GetProcessesByName("TabTip");
             foreach (Process p in tabProcesses) {
@@ -286,6 +317,7 @@ namespace STAAC {
         }
 
         private void BtnEdit_Click(object sender, EventArgs e) {
+            // Toggle editing mode:
             editMode = !editMode;
             if (editMode) {
                 btnEdit.Text = "Sav&e Changes";
@@ -299,6 +331,8 @@ namespace STAAC {
         }
 
         private void PnlButtons_Resize(object sender, EventArgs e) {
+
+            // Redraw the matrix of buttons whenever the form is resized:
             foreach (string line in File.ReadAllLines(Path.Combine(Application.StartupPath, MenuForm.TEMPLATE_FOLDER_NAME, MenuForm.selectedTemplate, MenuForm.SETTINGS_FILE_NAME))) {
                 if (line.ToLower().Contains("matrix data")) {
                     ReloadButtons(bm.GetMatrixData());
@@ -311,11 +345,15 @@ namespace STAAC {
         }
 
         private void BtnSpeak_Click(object sender, EventArgs e) {
+
+            // Create a Speech object to pronounce words based on their text contents:
             narrator = new SpeechSynthesizer();
             if (emphasizable.Contains(txtSpeechBar.Text)) {
                 narrator.SpeakSsmlAsync("<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\"><emphasis>" + txtSpeechBar.Text + "</emphasis></speak>");
             } else {
                 narrator.SpeakAsync(txtSpeechBar.Text);
+
+                // Add the word to emphasizable list, which is also removed from said list after a few seconds:
                 emphasizable.Add(txtSpeechBar.Text);
                 coolDown.Start();
             }
@@ -328,12 +366,16 @@ namespace STAAC {
         }
 
         private void BtnRepeat_Click(object sender, EventArgs e) {
+
+            // Repeat the last phrase just spoken:
             txtSpeechBar.Text = lastPhraseSpoken;
             txtSpeechBar.Text = txtSpeechBar.Text.Trim();
             btnSpeak.PerformClick();
         }
 
         private void TxtSpeechBar_KeyDown(object sender, KeyEventArgs e) {
+
+            // Use Enter to trigger the Speak button:
             if (e.KeyCode == Keys.Enter) {
                 e.Handled = true;
                 btnSpeak.PerformClick();
